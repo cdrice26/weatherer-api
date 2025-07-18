a3adb4333daf0ebaaa494a6adf38e6f1ccbc33f5
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-07-18 18:50:38 -0400

    Set up vercel

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Express } from 'express';
+
+const server: Express = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalPipes(new ValidationPipe());
+  await app.init();
 
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  if (require.main === module) {
+    // Running locally with `node main.ts`
+    await app.listen(3000);
+    console.log(`App running locally on: ${await app.getUrl()}`);
+  }
 }
+
 bootstrap();
+
+export default server; // Used by Vercel dev server
