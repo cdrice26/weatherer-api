c01b9fcc583cd9142f4bf1826ddf33b79396f1c0
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-07-27 18:55:23 -0400

    Added more error handling

--- src/regression/regression.service.ts
+++ src/regression/regression.service.ts
@@ -21,40 +21,44 @@ export class RegressionService {
     fields: string[],
     alpha: number
   ): Promise<Regression> {
-    const promises = fields.map(async (field) => {
-      const years = data.map((item) => item.year);
-      const fieldData = data.map((item) => item[field]);
-      const response = await fetch(process.env.REGRESSION_API_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          x: years,
-          y: fieldData,
-          degree: regressionDegree
-        })
-      });
+    try {
+      const promises = fields.map(async (field) => {
+        const years = data.map((item) => item.year);
+        const fieldData = data.map((item) => item[field]);
+        const response = await fetch(process.env.REGRESSION_API_URL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            x: years,
+            y: fieldData,
+            degree: regressionDegree
+          })
+        });
 
-      if (!response.ok) {
-        throw new Error('Failed to perform regression analysis');
-      }
-
-      const result = await response.json();
-      return {
-        coefficients: result.coefficients,
-        rSquared: result.r_squared,
-        testResults: {
-          pValue: result.test_results.p_value,
-          significant: result.test_results.p_value < alpha,
-          fStatistic: result.test_results.f_stat
+        if (!response.ok) {
+          throw new Error('Failed to perform regression analysis');
         }
-      };
-    });
-    const regressionResults = await Promise.all(promises);
 
-    return Object.fromEntries(
-      fields.map((field, index) => [field, regressionResults[index]])
-    );
+        const result = await response.json();
+        return {
+          coefficients: result.coefficients,
+          rSquared: result.r_squared,
+          testResults: {
+            pValue: result.test_results.p_value,
+            significant: result.test_results.p_value < alpha,
+            fStatistic: result.test_results.f_stat
+          }
+        };
+      });
+      const regressionResults = await Promise.all(promises);
+
+      return Object.fromEntries(
+        fields.map((field, index) => [field, regressionResults[index]])
+      );
+    } catch (e) {
+      throw new Error('Failed to perform regression analysis.');
+    }
   }
 }
